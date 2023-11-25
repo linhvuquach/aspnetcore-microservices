72de2f9b8fa3b4cf52c3d36477c57a5439cd020b
linhvuquach <quachvulinhag1997@gmail.com>
2023-11-25 22:41:54 +0700

    [ES-32]-Discount feat: migrate PostgreSQL database when Discount microservice startup

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 using Microsoft.OpenApi.Models;
 
@@ -26,5 +27,7 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.UseHttpsRedirection();
+
+app.MigrateDatabase<Program>();
 
 app.Run();
