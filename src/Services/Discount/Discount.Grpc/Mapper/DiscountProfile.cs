3c6d427d925e0b11b379af78545b03c54d41c28c
linhvuquach <quachvulinhag1997@gmail.com>
2023-11-28 17:27:46 +0700

    [ES-39]-Discount feat: implement AutoMapper into DiscountService of Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc;
+
+public class DiscountProfile : Profile
+{
+    protected DiscountProfile()
+    {
+        CreateMap<Coupon, CouponModel>().ReverseMap();
+    }
+}
