0a2322e7a9d7ab465601ddb89926ff0e7836a1ec
linhvuquach <quachvulinhag1997@gmail.com>
2023-11-28 17:29:55 +0700

    [ES-40]-Discount feat: develop CRUD in DiscountService to implement CRUD Grpc Proto service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -5,14 +5,14 @@ using Discount.Grpc.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
-
+builder.Services.AddAutoMapper(typeof(Program));
 // Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.MigrateDatabase<Program>();
