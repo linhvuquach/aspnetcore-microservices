8f73bf10f07c8506553137bdb8c5f99b116ee37e
linhvuquach <quachvulinhag1997@gmail.com>
2023-11-28 15:29:03 +0700

    [ES-35]-Discount feat: Managing PosgreSQL database operations Discount.Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,10 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -10,5 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MigrateDatabase<Program>();
 
 app.Run();
