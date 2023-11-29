01ddd2c926d914cc71e42fea91bf25a7a6359e6e
linhvuquach <quachvulinhag1997@gmail.com>
2023-11-29 22:16:57 +0700

    [ES-42]-Basket feat: register Discount.Grpc client and DiscountGrpc service

--- src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -6,7 +6,7 @@ namespace Discount.Grpc;
 
 public class DiscountProfile : Profile
 {
-    protected DiscountProfile()
+    public DiscountProfile()
     {
         CreateMap<Coupon, CouponModel>().ReverseMap();
     }
